512d4152c0cc915f31442052f921b49ca6818f0e
maksim <maks11m123@gmail.com>
2017-05-20 16:56:23 +0300

    update

--- js/script.js
+++ js/script.js
@@ -3,22 +3,35 @@ function createUrlPath(params){
 }
 
 $(document).ready(function(){
+    var preloader = {
+        selector: $('.preloader'),
+        hide: function(){
+            this.selector.fadeOut();
+        },
+        show: function(){
+            this.selector.fadeIn();
+        }
+    }
+
     function getPosts(){
+        preloader.show();
         $.get(createUrlPath('posts'), function(data){
-            $('#posts').append(data.map(function(item){
+            $('#posts').html(data.map(function(item){
                 return `
             <div class="col-md-3 text-center">
                 <h3 class="text-uppercase text-left"> ${item.title} </h3>
                 <img src="${item.img}" class="img-responsive" alt="">
                 <p> Likes: ${item.likes} </p>
             </div>
-                
                 `
             }).join(' '))
+            preloader.hide();
         })
     }
 
     getPosts();
+    // getPosts();
+    // getPosts();
 
 });
 
