9f185b37449811b67788a4b199ab1a5b412641f5
maksim <maks11m123@gmail.com>
2017-05-20 17:41:42 +0300

    added form validation

--- js/script.js
+++ js/script.js
@@ -13,6 +13,40 @@ $(document).ready(function(){
         }
     }
 
+    var form = $('#add-post'),
+        form_fields = form.find('input');
+
+    var formats = ['.jpg', '.png', '.gif'];
+
+    function addPost(e){
+        e.preventDefault();
+        var data = {};
+        form_fields.each(function(index, item){
+            var $element = $(item),
+                name = $element.attr('name'),
+                value = $element.val();
+
+            data[name] = value;
+        });
+
+        
+        var imgFormat = data.img.slice(-4);
+        formats.indexOf(imgFormat) !== -1 ? 
+        $.ajax({
+            method: 'POST',
+            url: createUrlPath('addpost'),
+            data: JSON.stringify($.extend( data, {id: Date.now(), likes: 0} )),
+            success: function(data){
+                if (data.status) {
+                    getPosts();
+                    $.growl.notice({ message: "Post added!" });
+                }
+            }
+        }) :  $.growl.error({ message: "Mistake in img url!" });
+    }
+
+    form.on('submit', addPost);
+
     function getPosts(){
         preloader.show();
         $.get(createUrlPath('posts'), function(data){
@@ -30,9 +64,6 @@ $(document).ready(function(){
     }
 
     getPosts();
-    // getPosts();
-    // getPosts();
-
 });
 
 
